6ae737031e11131fb47e4b4b275d3a938bbd078b
Farida <velizadefarida@gmail.com>
2026-03-25 15:02:05 +0300

    3 commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include "pe_vector.hpp"
+using knk::Vector;
 
 bool test1()
 {
   knk::Vector< int > v;
   return true;
 }
+bool test2() {
+  Vector< int > v;
+  return v.isEmpty();
+}
 
 int main()
 {
   using test_t = bool(*)();
   test_t tests[] = {
-    test1
+    test1,
+    test2
   };
   constexpr size_t count = sizeof(tests) / sizeof(test_t)
   for (size_t i = 0; i < count; ++i) {
--- pe-vector.hpp
+++ pe-vector.hpp
@@ -1,5 +1,6 @@
 #ifndef PE_VECTOR_HPP
-#DEFINE PE_VECTOR_HPP
+#define PE_VECTOR_HPP
+
 namespace knk
 {
   template< class T >
@@ -8,10 +9,23 @@ namespace knk
     Vector();
     ~Vector();
 
+    bool isEmpty() const noexcept;
+
    private:
     T* data;
     size_t size, capacity;
   };
 }
+
+template< class T >
+knk::Vector< T >::~Vector() {
+  delete [] data_;
+}
+template< class T >
+knk::Vector< T >::Vector() :
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
+{}
 
 #endif
