07d874ecc130e82e2c394f24aa1c683d9bf31c26
Farida <velizadefarida@gmail.com>
2026-03-25 15:53:30 +0300

    getSize for empty vector

--- main.cpp
+++ main.cpp
@@ -2,30 +2,51 @@
 #include "pe-vector.hpp"
 using knk::Vector;
 
-bool testConstructAndDestruct(const char ** pname) {
+bool testConstructAndDestruct(const char ** pname)
+{
   *pname = __func__;
   knk::Vector< int > v;
   return true;
 }
-bool testDefaultVectorIsEmpty(const char ** pname) {
+bool testDefaultVectorIsEmpty(const char ** pname)
+{
   *pname = __func__;
   Vector< int > v;
   return v.isEmpty();
+}
+bool testSizeOfEmptyVector(const char** pname)
+{
+  *pname = __func__;
+  Vector< int > v;
+  return !v.getSize();
+}
+bool testSizeOfNonEmptyVector(const char** pname)
+{
+  *pname = __func__;
+  return false;
 }
 
 int main()
 {
   using test_t = bool(*)(const char **);
-  test_t tests[] = {
-    testConstructAndDestruct,
-    testDefaultVectorIsEmpty
+  using case_t = std::pair< test_t, const char * >;
+  case_t tests[] = {
+    { testConstructAndDestruct, "Vector must be default constructable" },
+    { testDefaultVectorIsEmpty, "Default constructed vector must be empty" },
+    { testSizeOfEmptyVector, "Size of empty vector must be zero" },
+    { testSizeOfNonEmptyVector, "Size of non-empty vector must be positive" }
   };
-  constexpr size_t count = sizeof(tests) / sizeof(test_t)
+  constexpr size_t count = sizeof(tests) / sizeof(case_t)
+  size_t failed = 0;
   for (size_t i = 0; i < count; ++i) {
     const char * testName = nullptr;
-    bool r = tests[i](&testName);
+    bool r = tests[i].first(&testName);
     if (!r) {
-      std::cout << "Failed" << testName << "\n";
+      ++failed;
+      std::cout << "[FAIL]" << testName << "\n";
+      std::cout << "\t" << tests[i].second << "\n";
     }
   }
+  std::cout << "Summary: " << (count - failed) << " passed\n";
+  std::cout << "\t" << count << "total\n";
 }
--- pe-vector.hpp
+++ pe-vector.hpp
@@ -1,34 +1,40 @@
 #ifndef PE_VECTOR_HPP
 #define PE_VECTOR_HPP
+#include <cstddef>
 
 namespace knk
 {
   template< class T >
   class Vector {
    public:
-    Vector();
     ~Vector();
+    Vector();
 
     bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;
 
    private:
     T* data;
-    size_t size, capacity;
+    size_t size_, capacity_;
   };
 }
 template< class T >
-bool knk::Vector< T >::isEmpty() const noexcept {
-  return false;
+size_t knk::Vector< T > ::getSize() const noexcept {
+  return -1;
 }
 template< class T >
-knk::Vector< T >::Vector() :
+bool knk::Vector< T >::isEmpty() const noexcept {
+  return !size_;
+}
+template< class T >
+knk::Vector< T >::Vector():
   data_(nullptr),
   size_(0),
   capacity_(0)
 {
 template< class T >
 knk::Vector< T >::~Vector() {
-  delete [] data_;
+  delete[] data_;
 }
 
 #endif
